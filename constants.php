8a1b10070eb7863d2eefb19e73da305e8c0fa38d
FeSanz <felipe.antonio.condor@gmail.com>
2021-03-17 00:01:18 -0600

    ultimo servidor

--- constants.php
+++ constants.php
@@ -1,6 +1,18 @@
 <?php 
+// define('DB_HOST', 'localhost');
+// define('DB_USER', 'root');
+// define('DB_PASS', '');
+// define('DB_NAME', 'groundhumedity');
+
+     
+//define('DB_HOST', 'sql207.epizy.com');
+// define('DB_USER', 'epiz_27896249');
+// define('DB_PASS', 'R6zILtUSf6ua');
+// define('DB_NAME', 'epiz_27896249_ground_humedity');
+
+
  define('DB_HOST', 'localhost');
- define('DB_USER', 'root');
- define('DB_PASS', '');
- define('DB_NAME', 'groundhumedity');
+ define('DB_USER', 'id16398309_condor');
+ define('DB_PASS', 'Pdr~[8-S%R3+rIso');
+ define('DB_NAME', 'id16398309_humedity_sensor');
 
