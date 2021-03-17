1f9197a08653196f0e7a5ea1a2a0af55aec6b79f
FeSanz <felipe.antonio.condor@gmail.com>
2021-03-16 21:28:37 -0600

    size ajustado y cambio de rutas

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <?php require_once 'dboperations.php'; ?>
 <html lang="en">
 
@@ -8,30 +9,48 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="description" content="">
         <meta name="author" content="">
-        
-         <!--<meta http-equiv="refresh" content="5" />-->
+
+        <!--<meta http-equiv="refresh" content="5" />-->
 
         <title>Sensor humedad suelo</title>
 
         <!-- Custom fonts for this template-->
         <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-        <link
-            href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-            rel="stylesheet">
+        <link href="assets/css/fontsGoogle.css" rel="stylesheet" type="text/css"/>
 
         <!-- Custom styles for this template-->
         <link href="css/sb-admin-2.min.css" rel="stylesheet">
+        <link href="assets/alertify/css/themes/default.rtl.min.css" rel="stylesheet" type="text/css"/>
+        <link href="assets/alertify/css/alertify.min.css" rel="stylesheet" type="text/css"/>
+        <!-- alertify -->
+        <script src="assets/alertify/alertify.js" type="text/javascript"></script>
+        <script src="assets/alertify/alertify.min.js" type="text/javascript"></script>
+        
+        <!--  jQuery -->
+        <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+        <link rel="stylesheet" href="/resources/demos/style.css">
+        <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+        <script src="assets/js/datepickerCustom.js" type="text/javascript"></script>
+
+        <!-- Bootstrap core JavaScript
+        <script src="assets/js/jQuery.js" type="text/javascript"></script> 
+
+        <script src="vendor/jquery/jquery.min.js"></script>
+        <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+        <script src="vendor/jquery-easing/jquery.easing.min.js"></script>-->
+        <script src="js/sb-admin-2.min.js"></script>
+        <script src="vendor/chart.js/Chart.min.js"></script>
 
 
-        <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-        <link href="assets/css/asptectTable.css" rel="stylesheet" type="text/css"/>
-
-        <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
-        <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-        <!------ Include the above in your HEAD tag ---------->
-        <script src="assets/js/filters.js" type="text/javascript"></script>
-
+        <script src="assets/js/gstaticLoaderCharts.js" type="text/javascript"></script>
+        <script src="assets/js/chartline.js" type="text/javascript"></script>
+        <script src="assets/js/chartgauje.js" type="text/javascript"></script>
     </head>
+
+
     <body id="page-top">
 
         <!-- Page Wrapper -->
@@ -41,7 +60,7 @@
             <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
                 <!-- Sidebar - Brand -->
-                <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+                <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
                     <div class="sidebar-brand-icon rotate-n-15">
                         <i class="fas fa-laugh-wink"></i>
                     </div>
@@ -53,7 +72,7 @@
 
                 <!-- Nav Item - Dashboard -->
                 <li class="nav-item">
-                    <a class="nav-link" href="dashboard.php">
+                    <a class="nav-link" href="index.php">
                         <i class="fas fa-fw fa-tachometer-alt"></i>
                         <span>Dashboard</span></a>
                 </li>
@@ -66,8 +85,8 @@
                     Utilidades
                 </div>
 
-             <li class="nav-item">
-                    <a class="nav-link" href="index.php">
+                <li class="nav-item">
+                    <a class="nav-link" href="tables.php">
                         <i class="fas fa-fw fa-table"></i>
                         <span>Tablas</span></a>
                 </li>
@@ -105,7 +124,7 @@
                             <li class="nav-item dropdown no-arrow">
                                 <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <span class="mr-2 d-none d-lg-inline text-gray-600 small">Felipe Antonio</span>
+                                    <span class="mr-2 d-none d-lg-inline text-gray-600 small">Condor</span>
                                     <img class="img-profile rounded-circle"
                                          src="img/undraw_profile.svg">
                                 </a>
@@ -137,54 +156,68 @@
                     </nav>
                     <!-- End of Topbar -->
 
-                    <!-- Contenido principal -->
+                    <!-- Begin Page Content -->
                     <div class="container-fluid">
-                        <!-- Tabla de datos de sensor de humedad -->
-                        <div class="container">
-                            <div class="">
-                                <h1 class="h3 mb-2 text-gray-800">Registro de humedad del suelo</h1>
-                                <div class="panel panel-primary filterable">
-                                    <div class="panel-heading">
-                                        <h3 class="panel-title">Humedad del suelo</h3>
-                                        <div class="pull-right">
-                                            <button class="btn btn-default btn-xs btn-filter"><span class="glyphicon glyphicon-filter"></span> Filtro</button>
-                                        </div>
-                                    </div>
-                                    <table class="table">
-                                        <thead>
-                                            <tr class="filters">
-                                                <th><input type="text" class="form-control" placeholder="#" disabled></th>
-                                                <th><input type="text" class="form-control" placeholder="Planta" disabled></th>
-                                                <th><input type="text" class="form-control" placeholder="Fecha/Hora" disabled></th>
-                                                <th><input type="text" class="form-control" placeholder="Rango" disabled></th>
-                                                <th><input type="text" class="form-control" placeholder="Porcentaje" disabled></th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <?php
-                                            $db = new DbOperation();
-                                            $responseHumedity = json_decode(json_encode($db->getHumedityDates('2021-03-05', '2021-03-05')));
-                                            foreach ($responseHumedity as $fila) {
-                                                ?>
-                                                <tr>
-                                                    <td><?php echo $fila->id; ?></td>
-                                                    <td><?php echo $fila->plant; ?></td>
-                                                    <td><?php echo $fila->date_time; ?></td>
-                                                    <td><?php echo $fila->rango; ?></td>
-                                                    <td><?php echo $fila->percentage; ?></td>
 
-                                                </tr>
-                                                <?php
-                                            }
-                                            ?>
-                                        </tbody>
-                                    </table>
+                        <!-- Page Heading -->
+
+                        <!-- Content Row -->
+                        <div class="row">
+
+                            <div class="col-xl-12 col-lg-7">
+
+                                  <!-- Manometro -->
+                                <div class="card shadow mb-4">
+                                    <div class="card-header py-3">
+                                        <h6 class="m-0 font-weight-bold text-primary">Humedad actual</h6>
+                                    </div>
+                                    <div class="card-body">
+                                        <div class="col-auto" id="chart_div" style="width: 1300px; height: 250px; overflow-x:auto;"></div>
+                                        <hr><span id="debugTextManometer"></span>
+                                    </div>
                                 </div>
+                                  
+                                <!-- Grafica -->
+                                <div class="card shadow mb-4">
+                                    <div class="card-header py-3">
+                                        <h6 class="m-0 font-weight-bold text-primary">Humedad registrada</h6>
+                                    </div>
+                                    <div class="card-body">
+                                        <label class="col-12" style="background-color:powderblue;">Seleccione las fechas que desea mostrar en la gráfica</label>
+                                        <br><br>
+                                        <form class="form-horizontal" name="formDates" role="form" enctype="multipar/form-data">
+                                            <div class="row">
+                                                <label class="col-1">Fecha inicio:</label>
+                                                <div class="col">
+                                                    <input type="text" id="startDateValue" class="form-control" required="required">
+                                                </div>
+                                                <label class="col-1">Fecha fin:</label>
+                                                <div class="col">
+                                                    <input type="text" id="endDateValue" class="form-control" required="required">
+                                                </div>
+
+                                               
+                                                <div class="btn-group pull-right">
+                                                    <button type="button" id="serachButton" class="btn btn-primary">Buscar</button>
+                                                </div>
+                                                
+                                            </div>
+                                            <br>
+                                                 <p style="color:red;" id="responseSearch"></p>
+                                        </form>
+                                        <div class="col-auto" id="curve_chart" style="width: 1300px; height: 500px; overflow-x:auto;"></div>
+                                        <hr><span id="debugTextGraphic"></span>
+                                    </div>  
+                                </div>
+
                             </div>
+
+                            <!-- Donut Chart -->
+
                         </div>
 
                     </div>
-                    <!-- /.Contenido principal -->
+                    <!-- /.container-fluid -->
 
                 </div>
                 <!-- End of Main Content -->
@@ -193,7 +226,7 @@
                 <footer class="sticky-footer bg-white">
                     <div class="container my-auto">
                         <div class="copyright text-center my-auto">
-                            <span>Copyright &copy; Condor 2020</span>
+                            <span>Copyright &copy; 2021</span>
                         </div>
                     </div>
                 </footer>
@@ -205,24 +238,11 @@
         </div>
         <!-- End of Page Wrapper -->
 
-
-
-        <!-- Bootstrap core JavaScript-->
-        <script src="vendor/jquery/jquery.min.js"></script>
-        <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-        <!-- Core plugin JavaScript-->
-        <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-
-        <!-- Custom scripts for all pages-->
-        <script src="js/sb-admin-2.min.js"></script>
-
-        <!-- Page level plugins -->
-        <script src="vendor/chart.js/Chart.min.js"></script>
-
-        <!-- Page level custom scripts -->
-        <script src="js/demo/chart-area-demo.js"></script>
-        <script src="js/demo/chart-pie-demo.js"></script>
-        <script src="js/demo/chart-bar-demo.js"></script>
+        <!-- Scroll to Top Button-->
+        <a class="scroll-to-top rounded" href="#page-top">
+            <i class="fas fa-angle-up"></i>
+        </a>
 
     </body>
+
+</html>
